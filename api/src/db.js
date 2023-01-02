0e96ae1b95457e7b87608c6ac67e0a1a9402274b
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-02 15:49:47 -0300

    proyecto terminado

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, API_KEY, DB_NAME, DB_PORT
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 // const dietFunction = require("./models/Diet")
 // const recipeFunction = require("./models/Recipe") 
